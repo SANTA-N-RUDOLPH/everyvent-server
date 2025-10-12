63cf2ac6d95dc3a99820f00817dff50621d3cfce
hyeblee <hyeb1224@naver.com>
2025-10-12 23:09:03 +0900

    feat: ErrorCode 및 ErrorResponse 추가

---
+++ src/main/java/kr/santanrudolph/everyvent/global/ErrorResponse.java
@@ -0,0 +1,16 @@
+package kr.santanrudolph.everyvent.global;
+
+public record ErrorResponse(
+    String code,
+    String message,
+    String status
+) {
+
+  public static ErrorResponse of(ErrorCode errorCode) {
+    return new ErrorResponse(
+        errorCode.getCode(),
+        errorCode.getMessage(),
+        "ERROR"
+    );
+  }
+}
