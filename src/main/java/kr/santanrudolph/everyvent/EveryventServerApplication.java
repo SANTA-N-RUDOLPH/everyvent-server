05a63bd521fceba0e8fb707cde208247e15c3894
hyeblee <hyeb1224@naver.com>
2025-10-12 18:05:25 +0900

    chore: 프로젝트 초기 세팅 및 help.md 추가

---
+++ src/main/java/kr/santanrudolph/everyvent/EveryventServerApplication.java
@@ -0,0 +1,13 @@
+package kr.santanrudolph.everyvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryventServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EveryventServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/santanrudolph/everyvent/EveryventServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.santanrudolph.everyvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryventServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
