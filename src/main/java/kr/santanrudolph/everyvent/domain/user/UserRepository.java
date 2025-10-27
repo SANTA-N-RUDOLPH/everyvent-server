1fdd836cea14b8a8fcc2b6ff44e167b49fc312fe
NYoungLEE <skdud0694@gmail.com>
2025-10-28 00:53:08 +0900

    feat: UserRepository에 역할별 전체 사용자 조회 메서드 추가

--- src/main/java/kr/santanrudolph/everyvent/domain/user/UserRepository.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/user/UserRepository.java
@@ -3,6 +3,7 @@ package kr.santanrudolph.everyvent.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -18,4 +19,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
 
+  List<User> findAllByRoleAndDeletedAtIsNull(Role role);
 }
