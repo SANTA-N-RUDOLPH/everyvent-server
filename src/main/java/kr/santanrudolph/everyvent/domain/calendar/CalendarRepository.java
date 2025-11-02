4e0c765c131819309024f188c54664586bae6928
NYoungLEE <skdud0694@gmail.com>
2025-11-02 12:26:45 +0900

    fix: 월 단위 캘린더 조회 시 userZone 차이로 인한 누락 문제 수정

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/CalendarRepository.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/CalendarRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface CalendarRepository extends JpaRepository<Calendar,Long> {
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 
     @Query("SELECT COUNT(c) FROM Calendar c " +
             "WHERE c.user.id = :userId " +
-            "AND c.startDate = :startDate " +
-            "AND c.endDate = :endDate " +
+            "AND c.startDate <= :endDate " +
+            "AND c.endDate >= :startDate " +
             "AND c.deletedAt IS NULL")
     long countByUserIdInMonth(
             @Param("userId") Long userId,
@@ -25,8 +25,8 @@ public interface CalendarRepository extends JpaRepository<Calendar,Long> {
 
     @Query("SELECT c FROM Calendar c " +
             "WHERE c.user.id = :userId " +
-            "AND c.startDate = :startDate " +
-            "AND c.endDate = :endDate " +
+            "AND c.startDate <= :endDate " +
+            "AND c.endDate >= :startDate " +
             "AND c.deletedAt IS NULL")
     List<Calendar> findAllByUserIdInMonth(
             @Param("userId") Long userId,
@@ -37,8 +37,8 @@ public interface CalendarRepository extends JpaRepository<Calendar,Long> {
     @Query("SELECT c FROM OriginalCalendar c " +
             "WHERE c.isOfficial = true AND c.user.role = :role " +
             "AND c.user.id <> :userId " +
-            "AND c.startDate = :startDate " +
-            "AND c.endDate = :endDate " +
+            "AND c.startDate <= :endDate " +
+            "AND c.endDate >= :startDate " +
             "AND c.deletedAt IS NULL")
     List<Calendar> findAllOfficialCalendars(
             @Param("userId") Long userId,
