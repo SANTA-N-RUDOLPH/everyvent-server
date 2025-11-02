a01f258c49903741bc273f1ee8a56d1f32e834bf
hyeblee <hyeb1224@naver.com>
2025-11-02 23:17:55 +0900

    refactor: TokenRedisConfig의 RedisTemplate 타입을 <String, String>으로 단순화
    - 토큰은 문자열이므로, <String,Object> -> <String, String> 변경
    - RedisConfig -> TokenRedisConfig로 이름 변경

--- src/main/java/kr/santanrudolph/everyvent/global/config/TokenRedisConfig.java
+++ src/main/java/kr/santanrudolph/everyvent/global/config/TokenRedisConfig.java
@@ -7,11 +7,10 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
-public class RedisConfig {
+public class TokenRedisConfig {
 
   @Value("${spring.data.redis.host}")
   private String host;
@@ -28,15 +27,15 @@ public class RedisConfig {
   }
 
   @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
-    RedisTemplate<String, Object> template = new RedisTemplate<>();
+  public RedisTemplate<String, String> redisTemplate() {
+    RedisTemplate<String, String> template = new RedisTemplate<>();
     template.setConnectionFactory(redisConnectionFactory());
 
     template.setKeySerializer(new StringRedisSerializer());
     template.setHashKeySerializer(new StringRedisSerializer());
 
-    template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-    template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+    template.setKeySerializer(new StringRedisSerializer());
+    template.setHashKeySerializer(new StringRedisSerializer());
 
     template.afterPropertiesSet();
     return template;
