ea35ecde50edecaec1ce6f2f1fa660a3d9aac233
NYoungLEE <skdud0694@gmail.com>
2025-11-03 20:27:29 +0900

    feat: CalendarRequest DTO에 제목 및 설명 글자수 제한 추가

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/dto/request/CalendarRequest.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/dto/request/CalendarRequest.java
@@ -2,6 +2,7 @@ package kr.santanrudolph.everyvent.domain.calendar.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import kr.santanrudolph.everyvent.domain.calendar.enums.Category;
 import kr.santanrudolph.everyvent.domain.calendar.enums.Visibility;
 import lombok.Getter;
@@ -12,8 +13,10 @@ import lombok.NoArgsConstructor;
 public class CalendarRequest {
 
     @NotBlank
+    @Size(max = 20, message = "제목은 최대 20자까지 입력할 수 있습니다.")
     private String title;
 
+    @Size(max = 150, message = "설명은 최대 150자까지 입력할 수 있습니다.")
     private String description;
 
     @NotNull
