3807c67c7d40237633b7f9714818f415de55ef52
hyeblee <hyeb1224@naver.com>
2025-11-07 00:44:36 +0900

    refactor: JwtTokenProvider 예외를 도메인 예외로 변환
    - getUserIdFromToken getExpirationDate의 JWT 예외를 EveryventExeption으로 throw
    - 상세한 에러 메시지로 디버깅 개선

--- src/main/java/kr/santanrudolph/everyvent/auth/security/JwtTokenProvider.java
+++ src/main/java/kr/santanrudolph/everyvent/auth/security/JwtTokenProvider.java
@@ -7,6 +7,8 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Jwts.SIG;
 import io.jsonwebtoken.security.Keys;
+import kr.santanrudolph.everyvent.global.exception.ErrorCode;
+import kr.santanrudolph.everyvent.global.exception.EveryventException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -61,12 +63,27 @@ public class JwtTokenProvider {
   }
 
   public Long getUserIdFromToken(String token) {
-    Claims claims = parseClaims(token);
-    return Long.parseLong(claims.getSubject());
+    try {
+      Claims claims = parseClaims(token);
+      return Long.parseLong(claims.getSubject());
+    } catch (ExpiredJwtException e) {
+      throw new EveryventException(ErrorCode.ACCESS_TOKEN_EXPIRED, "토큰이 만료되어 userId를 추출할 수 없습니다");
+    } catch (NumberFormatException e) {
+      throw new EveryventException(ErrorCode.INVALID_ACCESS_TOKEN, "토큰의 userId 형식이 올바르지 않습니다");
+    } catch (JwtException | IllegalArgumentException e) {
+      throw new EveryventException(ErrorCode.INVALID_ACCESS_TOKEN, "userId 추출 중 토큰 파싱에 실패했습니다");
+    }
   }
 
   public Date getExpirationDate(String token) {
-    return parseClaims(token).getExpiration();
+    try {
+      return parseClaims(token).getExpiration();
+    } catch (ExpiredJwtException e) {
+      // 만료된 토큰이라도 만료 시간은 가져올 수 있음
+      return e.getClaims().getExpiration();
+    } catch (JwtException | IllegalArgumentException e) {
+      throw new EveryventException(ErrorCode.INVALID_ACCESS_TOKEN, "만료 시간 추출 중 토큰 파싱에 실패했습니다");
+    }
   }
 
   public boolean validateToken(String token) {
