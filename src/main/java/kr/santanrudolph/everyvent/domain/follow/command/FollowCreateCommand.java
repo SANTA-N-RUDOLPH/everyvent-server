260b69ced8ba6fe939797b8ae7bd9c197abe4c89
hyeblee <hyeb1224@naver.com>
2025-11-13 10:18:40 +0900

    fix(follow): Long 비교를 equals로 변경

--- src/main/java/kr/santanrudolph/everyvent/domain/follow/command/FollowCreateCommand.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/follow/command/FollowCreateCommand.java
@@ -14,7 +14,7 @@ public record FollowCreateCommand(
       throw new EveryventException(ErrorCode.INVALID_INPUT, "팔로워 유저의 ID는 1 이상의 값이어야 합니다.");
     } else if (targetId <= 0) {
       throw new EveryventException(ErrorCode.INVALID_INPUT, "팔로우 대상 유저의 ID는 1 이상의 값이어야 합니다.");
-    } else if (followerId == targetId) {
+    } else if (followerId.equals(targetId)) {
       throw new EveryventException(ErrorCode.INVALID_INPUT, "자기 자신을 팔로우할 수 없습니다.");
     }
   }
