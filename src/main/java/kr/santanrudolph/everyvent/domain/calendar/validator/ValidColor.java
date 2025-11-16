ef54fbfb626ec757fe48f7c16ce9ea7057a95640
NYoungLEE <skdud0694@gmail.com>
2025-11-16 19:25:38 +0900

    refactor: ValidColor 어노테이션에서 @Documented 제거

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/validator/ValidColor.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/validator/ValidColor.java
@@ -4,7 +4,6 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
-@Documented
 @Constraint(validatedBy = HexColorValidator.class)
 @Target({ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
