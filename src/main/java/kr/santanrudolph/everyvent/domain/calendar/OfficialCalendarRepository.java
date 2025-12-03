f35f7e342e8596524a22aa3068ca2dd084473779
NYoungLEE <skdud0694@gmail.com>
2025-12-03 21:59:59 +0900

    chore(calendar): 불필요한 import문 제거

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/OfficialCalendarRepository.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/OfficialCalendarRepository.java
@@ -3,10 +3,8 @@ package kr.santanrudolph.everyvent.domain.calendar;
 import kr.santanrudolph.everyvent.domain.calendar.entity.OfficialCalendar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
