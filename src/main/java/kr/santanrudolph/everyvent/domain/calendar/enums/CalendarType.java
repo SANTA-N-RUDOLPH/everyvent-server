8d479053defc314cfb135493413af42ffeaff364
hyeblee <hyeb1224@naver.com>
2025-12-05 20:49:45 +0900

    chore: 레거시 calendar와 task 코드 삭제

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/enums/CalendarType.java
+++
@@ -1,8 +0,0 @@
-package kr.santanrudolph.everyvent.domain.calendar.enums;
-
-public enum CalendarType {
-  ORIGINAL,
-  OFFICIAL,
-  DISTRIBUTED,
-  SCRAPED
-}
--- src/main/java/kr/santanrudolph/everyvent/domain/task/command/TaskUpdateCommand.java
+++
@@ -1,6 +0,0 @@
-package kr.santanrudolph.everyvent.domain.task.command;
-
-public record TaskUpdateCommand(
-    String name,
-    Integer day
-) {}
