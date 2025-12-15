1a0da9c822d09efc6996dbbc6fc7786ac17b58b4
hyeblee <hyeb1224@naver.com>
2025-12-16 04:16:19 +0900

    feat(calendar): 캘린더 엔터티의 user필드에 nullable = false 추가

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/Calendar.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/Calendar.java
@@ -29,7 +29,7 @@ public class Calendar extends BaseEntity {
   private Long id;
 
   @ManyToOne
-  @JoinColumn(name = "user_id")
+  @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
   @Column(nullable = false)
