504560ad4e832cc017535b67786c0e59fba21d35
hyeblee <hyeb1224@naver.com>
2025-12-18 18:03:01 +0900

    chore: 사용하지 않는 import 정리

--- src/main/java/kr/santanrudolph/everyvent/domain/user/UserService.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/user/UserService.java
@@ -11,8 +11,6 @@ import kr.santanrudolph.everyvent.global.exception.EveryventException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
