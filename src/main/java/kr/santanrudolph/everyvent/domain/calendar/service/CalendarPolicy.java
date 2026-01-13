1afa29f0ab0db135d25dd83b37341baa54d0db7e
hyeblee <hyeb1224@naver.com>
2026-01-13 12:12:50 +0900

    chore: 오류 메시지에 공백 추가

--- src/main/java/kr/santanrudolph/everyvent/domain/calendar/service/CalendarPolicy.java
+++ src/main/java/kr/santanrudolph/everyvent/domain/calendar/service/CalendarPolicy.java
@@ -106,7 +106,7 @@ public class CalendarPolicy {
       validateDeleted(calendar);
       validateOwner(user, calendar);
     } catch (EveryventException e) {
-      throw new EveryventException(e.getErrorCode(), "캘린더를 삭제할 수 없습니다." + e.getDetail());
+      throw new EveryventException(e.getErrorCode(), "캘린더를 삭제할 수 없습니다. " + e.getDetail());
     }
   }
 
@@ -116,7 +116,7 @@ public class CalendarPolicy {
       validateDeleted(calendar);
       validateOwner(user, calendar);
     } catch (EveryventException e) {
-      throw new EveryventException(e.getErrorCode(), "캘린더를 삭제할 수 없습니다." + e.getDetail());
+      throw new EveryventException(e.getErrorCode(), "캘린더를 삭제할 수 없습니다. " + e.getDetail());
     }
   }
 
